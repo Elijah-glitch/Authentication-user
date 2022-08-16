8b9da30f66c7b11c154ebd68185c07a209b04392
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2022-08-16 11:50:31 +0430

    create SingUp component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Stack, Row, Col } from 'react-bootstrap';
+import SingUp from './components/SingUp';
 
 function App() {
   return (
     <Stack>
       <Row>
-        <Col className='bg-image' style={{ minHeight: "100vh" }} sm={7}>sm=8</Col>
-        <Col className='bg-white' style={{ minHeight: "100vh" }} sm={5}>sm=4</Col>
+        <Col className='bg-image' style={{ minHeight: "100vh" }} sm={7}></Col>
+        <Col className='bg-white' style={{ minHeight: "100vh" }} sm={5}>
+          <div style={{ minHeight: "100vh" }} className='d-flex justify-content-center mx-4 mt-5'>
+            <SingUp />
+          </div>
+        </Col>
       </Row>
     </Stack>
   );
---
+++ src/components/SingUp.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Form, Button } from "react-bootstrap";
+
+function SingUp() {
+  return (
+    <Form style={{ width: "100%" }}>
+      <h1 className="text-center mb-4">Sign Up</h1>
+      <Form.Group className="mb-3">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Confirm Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button
+        className="mt-3"
+        style={{ width: "100%" }}
+        variant="primary"
+        type="submit"
+      >
+        Submit
+      </Button>
+    </Form>
+  );
+}
+
+export default SingUp;
