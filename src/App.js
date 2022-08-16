838f1be9ed535f12c5d814dfd7843236fcd9174b
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2022-08-16 11:26:29 +0430

    create layout project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import React from 'react'
+import { Stack, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Stack>
+      <Row>
+        <Col className='bg-image' style={{ minHeight: "100vh" }} sm={7}>sm=8</Col>
+        <Col className='bg-white' style={{ minHeight: "100vh" }} sm={5}>sm=4</Col>
+      </Row>
+    </Stack>
   );
 }
 
