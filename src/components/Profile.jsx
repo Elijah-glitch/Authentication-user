1c933697b9f273827368f469a11b7ff2b28b5c6f
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2022-08-17 10:27:53 +0430

    add avatar to Profile component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Alert, Button, Stack } from "react-bootstrap";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import "./publicStyle.css";
 
 function Profile() {
   const [error, setError] = useState("");
@@ -19,16 +20,22 @@ function Profile() {
   };
 
   return (
-    <Stack>
+    <div className="w-100 d-flex flex-column gap-2 align-items-center">
       <h2 className="text-center mb-4">Profile</h2>
       {error && <Alert variant="danger">{error}</Alert>}
-      <p>
-        <strong>Email:</strong> {currentUser.email}
-      </p>
-      <Button className="mt-2" variant="danger" onClick={handleLogout}>
+      <img
+        className="user-image"
+        src="image/806962_user_512x512.png"
+        alt="user"
+      />
+      <div className="w-100 d-flex justify-content-center gap-2 mt-2">
+        <strong><span>Email :</span></strong>
+        <span>{currentUser.email}</span>
+      </div>
+      <Button className="w-100 mt-4" variant="danger" onClick={handleLogout}>
         Log Out
       </Button>
-    </Stack>
+    </div>
   );
 }
 
